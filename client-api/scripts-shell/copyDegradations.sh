968923f927290fe3cb5e63ef07def81551052c3f
André Carvalho <afacarvalho@yahoo.com.br>
2020-02-03 17:30:28 -0200

    Including task to copy all degradation features to the separate table.

---
+++ client-api/scripts-shell/copyDegradations.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# get global env vars from Docker Secrets
+export POSTGRES_USER=$(cat "$POSTGRES_USER_FILE")
+export POSTGRES_PASS=$(cat "$POSTGRES_PASS_FILE")
+
+# Configs for database connect
+HOST=$POSTGRES_HOST
+USER=$POSTGRES_USER
+PASS=$POSTGRES_PASS
+
+DB="DETER-B"
+
+LIST_COLUMNS="origin_gid, classname, quadrant, orbitpoint, date, date_audit, lot, sensor, satellite, areatotalkm, areamunkm, areauckm, county, uf, uc, geom, publish_month"
+
+INSERT="INSERT INTO terrabrasilis.degradations($LIST_COLUMNS) SELECT $LIST_COLUMNS FROM terrabrasilis.deter_table WHERE classname in ('DEGRADACAO','CS_DESORDENADO','CS_GEOMETRICO','CICATRIZ_DE_QUEIMADA') AND date_audit > (SELECT MAX(date_audit) FROM terrabrasilis.degradations)"
+export PGPASSWORD=$PASS
+psql -h $HOST -U $USER -d $DB -c "$INSERT"
--- client-api/scripts-shell/entry_point.sh
+++ client-api/scripts-shell/entry_point.sh
@@ -27,6 +27,7 @@ then
     /usr/local/scripts-shell/generateJSON.sh month_auth_d
     /usr/local/scripts-shell/generateSHP.sh
     /usr/local/scripts-shell/generateTerramaSHP.sh
+    /usr/local/scripts-shell/copyDegradations.sh
 
     export PROJECT_NAME=deter-cerrado
     /usr/local/scripts-shell/generateJSON.sh daily_d
