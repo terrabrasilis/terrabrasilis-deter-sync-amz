8204b8bd358f1b8325b17732fa2e748933ffcd67
André Carvalho <afacarvalho@yahoo.com.br>
2020-06-05 22:42:11 -0300

    Fix missing file extension

--- client-api/scripts-shell/generateJSON.sh
+++ client-api/scripts-shell/generateJSON.sh
@@ -58,7 +58,7 @@ then
 		mv $TARGET_DIR/$LAYER_NAME-new.json $TARGET_DIR/$LAYER_NAME.json
 		echo $DATE" - The file "$LAYER_NAME-old.json" was removed and the new file "$LAYER_NAME" was created." >> $LOG
 		# Insert the updated_date into JSON as attribute into JSON
-		/usr/local/scripts-shell/updateDate.sh "$TARGET_DIR/$LAYER_NAME"
+		/usr/local/scripts-shell/updateDate.sh "$TARGET_DIR/$LAYER_NAME.json"
 	else
 		rm $TARGET_DIR/$LAYER_NAME-new.json
 		mv $TARGET_DIR/$LAYER_NAME-old.json $TARGET_DIR/$LAYER_NAME.json
