f989a22e2ba3ffbb6085e824701c651cb3af74bb
André Carvalho <afacarvalho@yahoo.com.br>
2021-09-01 14:29:52 -0300

    Improve script for SHP export.

--- client-api/scripts-shell/generateSHP.sh
+++ client-api/scripts-shell/generateSHP.sh
@@ -28,41 +28,59 @@ then
   DB="deter_cerrado"
 
 	FILTER_PUBLIC="0.03"
-	FILTER_ALL="0.01"
-	QUERY_PUBLIC="SELECT $OUTPUT_COLUMNS FROM (SELECT origin_gid||'_curr' as gid, 'aviso' as classname, quadrant, path_row, view_date, created_date, sensor, satellite, areatotalkm, areauckm, uc, areamunkm, county as municipality,  uf, geom FROM deter_cerrado_mun_ucs WHERE created_date <= (SELECT date FROM public.deter_publish_date) UNION SELECT origin_gid||'_hist' as gid, 'aviso' as classname, quadrant, path_row, view_date, created_date, sensor, satellite, areatotalkm, areauckm, uc, areamunkm, county as municipality,  uf, geom FROM deter_cerrado_history) as tb1 WHERE tb1.areatotalkm >= "
-	QUERY_AUTH="SELECT $OUTPUT_COLUMNS, tb1.areatotalkm as areatotkm FROM (SELECT origin_gid||'_curr' as gid, 'aviso' as classname, quadrant, path_row, view_date, created_date, sensor, satellite, areatotalkm, areauckm, uc, areamunkm, county as municipality,  uf, geom FROM deter_cerrado_mun_ucs UNION SELECT origin_gid||'_hist' as gid, 'aviso' as classname, quadrant, path_row, view_date, created_date, sensor, satellite, areatotalkm, areauckm, uc, areamunkm, county as municipality,  uf, geom FROM deter_cerrado_history) as tb1 WHERE tb1.areatotalkm >= "
+	FILTER_AUTH="0.01"
+	QUERY_FILTER_PUBLIC="view_date <= (SELECT date FROM public.deter_publish_date) AND tb1.areatotalkm >= "
+	QUERY_FILTER_AUTH="view_date <= now()::date AND tb1.areatotalkm >= "
+
+	QUERY="(SELECT origin_gid||'_curr' as gid, 'DESMATAMENTO_CR' as classname, "
+	QUERY="${QUERY}quadrant, path_row, view_date, created_date, sensor, satellite, areatotalkm, areauckm, uc, areamunkm, "
+	QUERY="${QUERY}county as municipality, uf, ST_Multi(geom)::geometry(MultiPolygon,4674) as geom FROM deter_cerrado_mun_ucs "
+	QUERY="${QUERY}UNION "
+	QUERY="${QUERY}SELECT origin_gid||'_hist' as gid, 'DESMATAMENTO_CR' as classname, "
+	QUERY="${QUERY}quadrant, path_row, view_date, created_date, sensor, satellite, areatotalkm, areauckm, uc, areamunkm, "
+	QUERY="${QUERY}county as municipality, uf, ST_Multi(geom)::geometry(MultiPolygon,4674) as geom FROM deter_cerrado_history) as tb1 "
+	QUERY="${QUERY}WHERE "
+
+	SELECT_PUBLIC="SELECT $OUTPUT_COLUMNS FROM "
+	QUERY_PUBLIC="${SELECT_PUBLIC} ${QUERY} ${QUERY_FILTER_PUBLIC}"
+
+	SELECT_AUTH="SELECT $OUTPUT_COLUMNS, tb1.areatotalkm as areatotkm FROM "
+	QUERY_AUTH="${SELECT_AUTH} ${QUERY} ${QUERY_FILTER_AUTH}"
+
 else
-
-	if [ "$PROJECT_NAME" == "deter-fm" ];
+	# The warning file is found on the external volume where the generated files are being placed.
+	WARNING_FILE="warning_about_area.txt"
+	if [ ! -f $WORKSPACE_DIR/$WARNING_FILE ] ;
 	then
-  	DB="deter_forest_monitor"
-
-		FILTER_PUBLIC="0.01"
-		FILTER_ALL="0.01"
-  	QUERY_PUBLIC="SELECT $OUTPUT_COLUMNS FROM (SELECT gid, classname, quadrant, orbitpoint as path_row, image_date as view_date, sensor, satellite, areatotalkm, areamunkm, areauckm, county as municipality, uf, uc, geom FROM terrabrasilis.deter_processed_fm limit 1 ) as tb1 WHERE tb1.areatotalkm >= "
-		QUERY_AUTH="SELECT $OUTPUT_COLUMNS, tb1.areatotalkm as areatotkm FROM (SELECT gid, classname, quadrant, orbitpoint as path_row, image_date as view_date, sensor, satellite, areatotalkm, areamunkm, areauckm, county as municipality, uf, uc, geom FROM terrabrasilis.deter_processed_fm ) as tb1 WHERE tb1.areatotalkm >= "
-	else
-		# The warning file is found on the external volume where the generated files are being placed.
-		WARNING_FILE="warning_about_area.txt"
-		if [ ! -f $WORKSPACE_DIR/$WARNING_FILE ] ;
-		then
-			cp -a "$SCRIPTS_BASE_PATH/$WARNING_FILE" "$WORKSPACE_DIR/$WARNING_FILE"
-		fi;
-
-		DB="DETER-B"
-
-		FILTER_PUBLIC="0.0625"
-		FILTER_ALL="0.01"
-		ALL_FILTER="uf != ('MS') AND date > (SELECT end_date FROM public.prodes_reference)"
-		PUBLIC_FILTER="$ALL_FILTER AND date <= (SELECT date FROM public.deter_publish_date)"
-  	QUERY_PUBLIC="SELECT $OUTPUT_COLUMNS FROM (SELECT gid||'_curr' as gid, classname, quadrant, orbitpoint as path_row, date as view_date, lot, sensor, satellite, areatotalkm, areamunkm, areauckm, county as municipality, uf, uc, geom FROM deter_table WHERE $PUBLIC_FILTER UNION SELECT gid||'_hist', classname, quadrant, orbitpoint as path_row, date as view_date, lot, sensor, satellite, areatotalkm, areamunkm, areauckm, county as municipality, uf, uc, geom FROM deter_history) as tb1 WHERE tb1.areatotalkm >= "
-		QUERY_AUTH="SELECT $OUTPUT_COLUMNS, tb1.areatotalkm as areatotkm FROM (SELECT gid||'_curr' as gid, classname, quadrant, orbitpoint as path_row, date as view_date, lot, sensor, satellite, areatotalkm, areamunkm, areauckm, county as municipality,  uf, uc, geom FROM deter_table WHERE $ALL_FILTER UNION SELECT gid||'_hist', classname, quadrant, orbitpoint as path_row, date as view_date, lot, sensor, satellite, areatotalkm, areamunkm, areauckm, county as municipality, uf, uc, geom FROM deter_history) as tb1 WHERE tb1.areatotalkm >= "
+		cp -a "$SCRIPTS_BASE_PATH/$WARNING_FILE" "$WORKSPACE_DIR/$WARNING_FILE"
 	fi;
+
+	DB="DETER-B"
+
+	FILTER_PUBLIC="0.0625"
+	FILTER_AUTH="0.01"
+	QUERY_FILTER_AUTH="uf != ('MS') AND view_date > (SELECT end_date FROM public.prodes_reference) AND tb1.areatotalkm >= "
+	QUERY_FILTER_PUBLIC="view_date <= (SELECT date FROM public.deter_publish_date) AND ${QUERY_FILTER_AUTH}"
+
+	QUERY="(SELECT gid||'_curr' as gid, classname, "
+	QUERY="${QUERY}quadrant, orbitpoint as path_row, date as view_date, lot, sensor, satellite, areatotalkm, areamunkm, "
+	QUERY="${QUERY}areauckm, county as municipality, uf, uc, geom FROM deter_table "
+	QUERY="${QUERY}UNION "
+	QUERY="${QUERY}SELECT gid||'_hist', classname, "
+	QUERY="${QUERY}quadrant, orbitpoint as path_row, date as view_date, lot, sensor, satellite, areatotalkm, areamunkm, "
+	QUERY="${QUERY}areauckm, county as municipality, uf, uc, geom FROM deter_history) as tb1 "
+	QUERY="${QUERY}WHERE "
+
+	SELECT_PUBLIC="SELECT $OUTPUT_COLUMNS FROM "
+	QUERY_PUBLIC="${SELECT_PUBLIC} ${QUERY} ${QUERY_FILTER_PUBLIC}"
+
+	SELECT_AUTH="SELECT $OUTPUT_COLUMNS, tb1.areatotalkm as areatotkm FROM "
+	QUERY_AUTH="${SELECT_AUTH} ${QUERY} ${QUERY_FILTER_AUTH}"
 fi;
 
 cd $WORKSPACE_DIR/
 
-pgsql2shp -f $WORKSPACE_DIR/deter_all -h $HOST -u $USER -P $PASS $DB "$QUERY_AUTH $FILTER_ALL"
+pgsql2shp -f $WORKSPACE_DIR/deter_all -h $HOST -u $USER -P $PASS $DB "$QUERY_AUTH $FILTER_AUTH"
 pgsql2shp -f $WORKSPACE_DIR/deter_public -h $HOST -u $USER -P $PASS $DB "$QUERY_PUBLIC $FILTER_PUBLIC"
 
 zip "all.zip" deter_all.shp deter_all.shx deter_all.prj deter_all.dbf $WARNING_FILE
