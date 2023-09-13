a6623fc39b6b69eb1d15bb4e2da3a16b701b5eff
andre-carvalho <afacarvalho@yahoo.com.br>
2023-09-13 16:24:24 -0300

    Change source ID column

--- client-api/scripts-shell/generateSHP.sh
+++ client-api/scripts-shell/generateSHP.sh
@@ -41,11 +41,11 @@ then
 	QUERY_FILTER_PUBLIC="view_date <= (SELECT date FROM public.deter_publish_date) AND tb1.areatotalkm >= "
 	QUERY_FILTER_AUTH="view_date <= now()::date AND tb1.areatotalkm >= "
 
-	QUERY="(SELECT origin_gid||'_curr' as gid, 'DESMATAMENTO_CR' as classname, "
+	QUERY="(SELECT gid||'_curr' as gid, 'DESMATAMENTO_CR' as classname, "
 	QUERY="${QUERY}quadrant, path_row, view_date, created_date, sensor, satellite, areatotalkm, areauckm, uc, areamunkm, "
 	QUERY="${QUERY}county as municipality, geocod as geocodibge, uf, ST_Multi(geom)::geometry(MultiPolygon,4674) as geom FROM deter_cerrado_mun_ucs "
 	QUERY="${QUERY}UNION "
-	QUERY="${QUERY}SELECT origin_gid||'_hist' as gid, 'DESMATAMENTO_CR' as classname, "
+	QUERY="${QUERY}SELECT gid||'_hist' as gid, 'DESMATAMENTO_CR' as classname, "
 	QUERY="${QUERY}quadrant, path_row, view_date, created_date, sensor, satellite, areatotalkm, areauckm, uc, areamunkm, "
 	QUERY="${QUERY}county as municipality, geocod as geocodibge, uf, ST_Multi(geom)::geometry(MultiPolygon,4674) as geom FROM deter_cerrado_history) as tb1 "
 	QUERY="${QUERY}WHERE "
@@ -71,7 +71,7 @@ else
 	QUERY_FILTER_AUTH="uf != ('MS') AND tb1.areatotalkm >= "
 	QUERY_FILTER_PUBLIC="view_date <= (SELECT date FROM public.deter_publish_date) AND ${QUERY_FILTER_AUTH}"
 
-	QUERY="(SELECT gid||'_curr' as gid, classname, "
+	QUERY="(SELECT id||'_curr' as gid, classname, "
 	QUERY="${QUERY}quadrant, orbitpoint as path_row, date as view_date, lot, sensor, satellite, areatotalkm, areamunkm, "
 	QUERY="${QUERY}areauckm, county as municipality, geocod as geocodibge, uf, uc, geom FROM deter_table "
 	QUERY="${QUERY}WHERE date > (SELECT end_date FROM public.prodes_reference) "
